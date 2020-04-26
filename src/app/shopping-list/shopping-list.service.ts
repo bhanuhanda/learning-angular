05ba5e41b462102356818b49c578ccf2f8b923ee
bhanuhanda <bhanuhanda98@gmail.com>
2020-04-26 17:45:54 +0530

    observables done

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,9 @@
 import { Ingredient } from "../shared/ingredient.model";
-import { EventEmitter } from "@angular/core";
+// import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   private ingredients: Ingredient[] = [
     new Ingredient("Apples", 5),
     new Ingredient("Tomatoes", 10),
@@ -14,7 +15,7 @@ export class ShoppingListService {
 
   addIngredient(ingredint: Ingredient) {
     this.ingredients.push(ingredint);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addToShoppingList(ingredients: Ingredient[]) {
@@ -22,5 +23,6 @@ export class ShoppingListService {
     //   this.shoppingListService.addIngredient(ingredient);
     // }
     this.ingredients.push(...ingredients);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
