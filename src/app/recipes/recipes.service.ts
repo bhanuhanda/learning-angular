55cb9e672ae59263b32f704cff7bc9fc936ca085
bhanuhanda <bhanuhanda98@gmail.com>
2020-04-29 18:33:09 +0530

    HTTP requests w/o authentication

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -7,23 +7,30 @@ import { Subject } from "rxjs";
 @Injectable()
 export class RecipesService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      "A test Recipe",
-      "This is simply a test",
-      "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg",
-      [new Ingredient("Meat", 1), new Ingredient("Fries", 20)]
-    ),
-    new Recipe(
-      "Second test Recipe",
-      "This is simply second test",
-      "https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg",
-      [new Ingredient("Bread", 4), new Ingredient("Suace", 1)]
-    ),
-  ];
+
+  //   private recipes: Recipe[] = [
+  //     new Recipe(
+  //       "A test Recipe",
+  //       "This is simply a test",
+  //       "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg",
+  //       [new Ingredient("Meat", 1), new Ingredient("Fries", 20)]
+  //     ),
+  //     new Recipe(
+  //       "Second test Recipe",
+  //       "This is simply second test",
+  //       "https://cdn.pixabay.com/photo/2018/12/22/16/36/recipe-3889916_1280.jpg",
+  //       [new Ingredient("Bread", 4), new Ingredient("Suace", 1)]
+  //     ),
+  //   ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
+  setRecipes(newRecipes: Recipe[]) {
+    this.recipes = newRecipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
   getRecipes() {
     // gives us the reference to the original array, but we dont want to pass that to outside
     // return this.recipes;
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { RecipesService } from "../recipes/recipes.service";
+import { Recipe } from "../recipes/recipes.model";
+import { map, tap } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class DataStorageService {
+  url: string =
+    "https://ng-recipe-book-project-290420.firebaseio.com/recipes.json";
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipesService
+  ) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(this.url, recipes).subscribe((res) => {
+      console.log(res);
+    });
+  }
+  fetchRecipes() {
+    return this.http.get<Recipe[]>(this.url).pipe(
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      tap((recipes) => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
+  }
+}
